e6829b639edcebf99a77af30fb856f46be41d95b
baotrung <baotrung.nguyen.10f@gmail.com>
2017-05-07 14:39:24 +0200

    modify model folder

--- app/src/main/java/mitru/com/mvvmapplication/model/Login.java
+++ app/src/main/java/mitru/com/mvvmapplication/model/Login.java
@@ -1,5 +1,7 @@
 package mitru.com.mvvmapplication.model;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 /**
@@ -7,4 +9,5 @@ import java.io.Serializable;
  */
 
 public class Login implements Serializable {
+    @SerializedName("username") public String userName;
 }
