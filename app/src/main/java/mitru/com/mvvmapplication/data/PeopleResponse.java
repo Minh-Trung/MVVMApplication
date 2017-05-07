0e86dee7b7346f82a0f814594c50a95f16d3c554
baotrung <baotrung.nguyen.10f@gmail.com>
2017-05-07 16:46:02 +0200

    commit

--- app/src/main/java/mitru/com/mvvmapplication/data/PeopleResponse.java
+++ app/src/main/java/mitru/com/mvvmapplication/data/PeopleResponse.java
@@ -1,8 +1,23 @@
 package mitru.com.mvvmapplication.data;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import mitru.com.mvvmapplication.model.People;
+
 /**
  * Created by ctruong on 07/05/2017.
  */
 
 public class PeopleResponse {
+    @SerializedName("results") private List<People> peopleList;
+
+    public List<People> getPeopleList() {
+        return peopleList;
+    }
+
+    public void setPeopleList(List<People> peopleList) {
+        this.peopleList = peopleList;
+    }
 }
--- app/src/main/java/mitru/com/mvvmapplication/data/PeopleService.java
+++ app/src/main/java/mitru/com/mvvmapplication/data/PeopleService.java
@@ -1,8 +1,16 @@
 package mitru.com.mvvmapplication.data;
 
+
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
 /**
  * Created by ctruong on 07/05/2017.
  */
 
 public interface PeopleService {
+    @GET
+    Observable<PeopleResponse> fetchPeople(@Url String url);
 }
